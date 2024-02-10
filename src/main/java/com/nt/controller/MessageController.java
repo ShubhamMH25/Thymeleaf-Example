a9b6450e838f6f8b2861c7c26f52ac8279ef254c
Shubham <shubhamlangade3@gmail.com>
2024-02-10 20:10:56 +0530

    initial commit

---
+++ src/main/java/com/nt/controller/MessageController.java
@@ -0,0 +1,19 @@
+package com.nt.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+
+@Controller
+public class MessageController {
+
+	@GetMapping("/welcome")
+	public String getWelcomeMsg(Model model)
+	{
+		model.addAttribute("msg","Welcome to another new technology");
+		
+		return "index";
+	}
+}
